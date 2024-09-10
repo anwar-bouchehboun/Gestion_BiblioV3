108bf4250fb0b1d9d8e8972bd6876a3b02979aea
anouar <anouar.ab95@gmail.com>
2024-09-10 16:50:20 +0100

    update strcture

--- src/main/java/org/gestionBibliothique/Main.java
+++ src/main/java/org/gestionBibliothique/Main.java
@@ -15,18 +15,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
        JournalTheseUI j=   new JournalTheseUI();
         j.stratJournal();
-    /*  ThèseUniversitaire d= new ThèseUniversitaire();
-d.setAuteur("aaaa");
-d.setTitre("fffff");
-        System.out.print("Enter publication date (yyyy-MM-dd): ");
-        LocalDate datePublication = LocalDate.parse(scanner.nextLine());
-d.setDatePublication(datePublication);
-d.setNombreDePages(333);
-d.setType(TypeDocument.THESE_UNIVERSITAIRE);
-d.setUniversite("Youcode");
-TheseUniversitaireDao dao = new TheseUniversitaireDao();
-
-dao.create(d);*/
 
 
 
