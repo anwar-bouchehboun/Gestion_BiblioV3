73d2818e177c9d5dd3e291ec3a6ff39236c7b9a2
anouar <anouar.ab95@gmail.com>
2024-09-11 16:32:11 +0100

    update main

--- src/main/java/org/gestionBibliothique/Main.java
+++ src/main/java/org/gestionBibliothique/Main.java
@@ -16,38 +16,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Scanner scanner = new Scanner(System.in);
-  /*   JournalTheseUI j=   new JournalTheseUI();
-        j.stratJournal();*/
-     /*    UserUI u= new UserUI();
-        u.Strat();
-*/
+
         MagazinlivreUi m =new MagazinlivreUi();
         m.stratMenu();
 
 
 
-     /*   System.out.print("Enter ID : ");
-        int id = Integer.parseInt(scanner.nextLine());
-        System.out.print("Enter title: ");
-        String titre = scanner.nextLine();
 
-        System.out.print("Enter author: ");
-        String auteur = scanner.nextLine();
-
-        System.out.print("Enter publication date (yyyy-MM-dd): ");
-        LocalDate datePublication = LocalDate.parse(scanner.nextLine());
-
-        System.out.print("Enter number of pages: ");
-        int nombreDePages = Integer.parseInt(scanner.nextLine());
-
-   System.out.print("Enter document type (MAGAZINE,LIVRE,JOURNAL_SCIENTIFIQUE,THESE_UNIVERSITAIRE): ");
-        TypeDocument type = TypeDocument.valueOf(scanner.nextLine().toUpperCase());
-
-        System.out.print("Enter research domain: ");
-        String domaineRecherche = scanner.nextLine();
-
-*/
 
 
 
